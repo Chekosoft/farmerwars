ff199cc300a866ef146fa3c6ff0a0497bf6004c9
Joaquín Muñoz M <jemunozmunoz@gmail.com>
2015-12-07 15:59:32 -0300

    added status text

---
+++ src/objects/StatusText.js
@@ -0,0 +1,20 @@
+export default class StatusText extends Phaser.Text {
+
+    constructor(game, x, y, player) {
+        super(game, x, y, "asdf",
+        {
+            font: "14px Droid Sans",
+            align: "left",
+            fill : "#ffffff"
+        });
+        this.player = player;
+        this.game.stage.addChild(this);
+    }
+
+    updateStatus(player) {
+        var playerStatus = `Player Type: ${player.type}, Is resting: ${(player.isTired) ? "Yes" : "No"}
+Energy: ${player.stamina}, Score: ${player.score}, Can move? ${player.canMove}`;
+
+this.setText(playerStatus);
+    }
+}
